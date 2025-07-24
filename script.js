e494d312bbc7707d2fde1a82b59cfca2d03287d2
dimasjp <diims.el@gmail.com>
2025-07-24 12:49:22 +0700

    Add function generateGrid to generate the grid and a fixed css styling

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const board = document.querySelector('.container');
 
-function createBoard() {
-    let boardSize = 256;
-
-    for (let i = 0; i < boardSize; i++) {
+function generateGrid(num) {
+    for (let i = 0; i < num * num; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
         board.appendChild(box);
-        i++;
     }
 }
 
+generateGrid(16);  
