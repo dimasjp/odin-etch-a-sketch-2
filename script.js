11feaa97fd7dcc14dcbce9cbb89e6d7421d9d302
dimasjp <diims.el@gmail.com>
2025-07-25 12:51:17 +0700

    Fix generateGrid sizing

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const board = document.querySelector('.container');
+const btnGenerate = document.querySelector('.btn-generate');
+
+btnGenerate.addEventListener('click', () => {
+    let num = prompt('Grid Size');
+    
+    if (num > 100) {
+        alert("Grid size should be 100 or smaller!")
+    } else {
+        generateGrid(num);
+    }
+})
 
 function generateGrid(num) {
-    for (let i = 0; i < num * num; i++) {
+    board.replaceChildren();
+
+    for (let i = 1; i <= num * num; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
         board.appendChild(box);
+
+        let boxWidth = 100 / num;
+        box.style.minWidth = `${boxWidth}%`;
 
         box.addEventListener('mouseover', () => {
             box.style.backgroundColor = 'black';
         })
     }
 }
-
-
 
 generateGrid(16);
